a182223293e0be19326f31d1b6ede7a1fb53017f
TseHang <a54383813@gmail.com>
2016-11-06 15:33:11 +0800

    okgogoo init

--- route.js
+++ route.js
@@ -2,15 +2,29 @@
   It's all by  `canner-core` to transform
 */
 var route = [
-    {
-      data: {
-        path: './',
-        title: 'Hello guys',
-        first_word: 'It is a good template'
-      },
-      partials: './partial.js',
-      layout:  "./layout/index.hbs",
-      filename: "./index.html"
-    }
-  ];
+  {
+    data: {
+      path: './',
+      title: 'Hello guys',
+      first_word: 'It is a good template'
+    },
+    partials: './partial.js',
+    layout:  "./layout/index.hbs",
+    filename: "./index.html"
+  },{
+    data: {
+      path: './',
+    },
+    partials: './partial.js',
+    layout:  "./layout/donutChart.hbs",
+    filename: "./donutChart.html"
+  },{
+    data: {
+      path: './',
+    },
+    partials: './partial.js',
+    layout:  "./layout/map.hbs",
+    filename: "./map.html"
+  }
+];
 module.exports = route;
