5c06ac144ebc661f54e3aa35f6029271a941c596
a0952866367 <a0952866367@yahoo.com.tw>
2016-11-18 23:30:24 +0800

    Area Name Chinese/English

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 var service_name = {"違規停車": 0,"路燈故障": 1 ,"噪音舉發": 2,"騎樓舉發": 3,"道路維修": 4,"交通運輸": 5,"髒亂及污染": 6,"民生管線": 7,"動物救援": 8 , "其他":9};
+var areaNameE = {
+    '新化區':'Xinhua', '新營區':'Xinying', '鹽水區':'Yanshui', '白河區':'Baihe',
+    '柳營區':'Liuying', '後壁區':'Houbi', '東山區':'Dongshan', '麻豆區':'Madou',
+    '下營區':'Xiaying', '六甲區':'Liujia', '官田區':'Guantian', '大內區':'Danei',
+    '佳里區':'Jiali', '學甲區':'Xuejia', '西港區':'Xigang', '七股區':'Qigu',
+    '將軍區':'Jiangjun', '北門區':'Beimen', '善化區':'Shanhua', '新市區':'Xinshi',
+    '安定區':'Anding', '山上區':'Shanshang', '玉井區':'Yujing', '楠西區':'Nanxi',
+    '南化區':'Nanhua', '左鎮區':'Zuozhen', '仁德區':'Rende', '歸仁區':'Guiren',
+    '關廟區':'Guanmiao', '龍崎區':'Longqi', '永康區':'Yongkang', '東區':'East',
+    '南區':'South', '北區':'North' , '中西區':'WestCentral', '安南區':'Annan',
+    '安平區':'Anping' , '全區':'All'
+};
 
 function filtJSON(json, key, value) {
     var count = 0;
@@ -46,8 +58,12 @@ d3.json("fake_areas.json", function(error, data){
         temp.push(9);//其他歸類在第9項
         temp.push(sum);//事件發生數量
         temp.push("其他");
+        var nameObj = {
+            cName: areaName,
+            eName: areaNameE[areaName]
+        }
         array.splice(3, count - 3, temp);//保留前三高，其餘加總到「其他」
-        createDonut( array , areaName , "column");//畫圖
+        createDonut( array , nameObj , "column");//畫圖
     })
 });
 
@@ -71,13 +87,16 @@ function createDonut(array, name, column_object){
     var arc3 = d3.svg.arc()
                     .innerRadius((width/3))
                     .outerRadius((width/2.5));
-    var arcs = svg.selectAll("g.arc")
-                .data(pie(
+
+    var pieData = pie(
                     array.map(//遍尋處理每個array element，並取得特定欄位 = 數量
-                        function(element,index,array) {
+                        function(element, index, array) {
                             return element[1];
-                        })
-                ))
+                    })
+                  )
+
+    var arcs = svg.selectAll("g.arc")
+                .data(pieData)
                 .enter()
                 .append("g")
                 .attr({
@@ -85,17 +104,16 @@ function createDonut(array, name, column_object){
                     "transform": "translate(" + (width/2) + ", " + (width/2) + ")",
                 });
 
-    var div = d3.select("body").append("div").attr("class", "toolTip");
+    var div = d3.select("body").append("div").attr("class","toolTip");
     var color = ['#E57373','#DCE775','#F06292','#BA68C8','#7986CB','#4DB6AC','#81C784','#FF8A65','#A1887F','#E0E0E0']
     var path = arcs.append("path")
                     .attr({
-                        "fill": function(d,i){
+                        "fill": function(d, i){
                             return color[array[i][0]];//color的array
                         },
                         "d": arc,
                         "class": function(d,i){
-                            //給最大的一點特效！！
-                            if(i == 0){
+                            if(i == 0){//最大的有特效
                                 return "path_special";
                             }
                             return "path";
@@ -104,13 +122,11 @@ function createDonut(array, name, column_object){
                             return array[i][2];
                         }
                     })
-                    .on("mousemove", function(d){
-                        console.log(d3.select(this));
-
+                    .on("mousemove", function(d, i){
                         div.style("left", d3.event.pageX+10+"px");
                         div.style("top", d3.event.pageY-25+"px");
                         div.style("display", "inline-block");
-                        div.html((d[2])+"<br>"+(d.data.value)+"%");
+                        div.html(array[i][2]+"<br>"+array[i][1]+"件");
                     })
                     .on("mouseout", function(d){
                         div.style("display", "none");
@@ -142,13 +158,13 @@ function createDonut(array, name, column_object){
 
     svg.append("text")//區域名稱，放在圖下
        .attr({
-            "id": "district",
+            "id": name.eName,
             "x": width/2 - 23,
             "y": height - 30
        })
        .style("font-size","20px")
        .text(function(d){
-            return name;
+            return name.cName;
        })
 
     var items = Object.keys(service_name);
