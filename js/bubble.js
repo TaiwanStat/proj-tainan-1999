09f289d01a6a021716e0262fc4675e9c4d0c46f2
kuoteng <rapirent>
2016-11-19 15:39:29 +0800

    fix slider bar

--- js/bubble.js
+++ js/bubble.js
@@ -57,30 +57,55 @@
 	option();
 	//使用moment.js來p解析時間
 	var select_date = moment("2016-11-09");//TODO:設定最早時間
+    var last_data = "2016-11-09";
 	var now_date = new moment();//取得當前時間
-	//以now_bar_value來紀錄當前bar的數值（因為bar是由sytle:"left:0%"決定
-	var now_bar_value = 0;
+	//slider_value save the slider percent
+	var slider_value = 0;
 	var duration_day = parseInt((now_date._d - select_date._d) / (1000 * 60 * 60 * 24));
 	var dateGOGO = d3.time.format("%Y-%m-%d");
-	d3.select('#scroll_bar').call(
-		d3.slider().min(0).max(duration_day).step(1)
-			.on("slide", function(evt, value) {
-                // console.log("entered!");
-				now_bar_value = value;
-				var duration_set = moment.duration({'days' : now_bar_value});
-				select_date = moment("2016-11-09").add(duration_set);
-                // console.log(dateGOGO(select_date._d));
-				d3.selectAll("#show_date").text(dateGOGO(select_date._d));
-				//改成call api
-	//			json_data = G.getItemsData('2016-11-09', dateGOGO(select_date._d), ['新化區', '新營區']);
-				d3.json("../../src/faked2.json", function(error, data){
-				 	if (error){
-				 		console.log(error);
-				 	}
-					change(data);
-				})
-			})
-	);
+    var slider = $("input");
+    slider.on('mouseenter',function(){
+            console.log(slider_value);
+            slider.on('click',function(){
+                slider_value = slider.val();
+                // console.log(slider_value);
+                change_slider(slider_value);
+            });
+            slider.on('mousemove',function(){
+                slider_value = slider.val();
+                // if(slider_value)
+                // console.log(slider_value);
+                // change_slider2(slider_value);
+            });
+          });
+    // function change_slider2(n){
+    //     slider.css({
+    //         'background-image':'-webkit-linear-gradient(left ,#222 0%,#222 '+ n +'%,#eee '+ n +'%, #eee 100%)'
+    //     });
+    // }
+    function change_slider(n){
+        slider.css({
+            'background-image':'-webkit-linear-gradient(left ,#222 0%,#222 '+ n +'%,#eee '+ n +'%, #eee 100%)'
+        });
+        var duration_set = moment.duration({'days' : (duration_day/100)*n});
+        // console.log((duration_day/100)*n + 1);
+        // console.log(n);
+        select_date = moment("2016-11-09").add(duration_set);
+        d3.selectAll("#show_date").text(dateGOGO(select_date._d));
+        if(last_data != dateGOGO(select_date._d))
+        {
+            //TODO
+                            //改成call api
+    //          json_data = G.getItemsData('2016-11-09', dateGOGO(select_date._d), ['新化區', '新營區']);
+            d3.json("../../src/faked2.json", function(error, data){
+                if (error){
+                    console.log(error);
+                }
+                change(data);
+            })
+            last_data = dateGOGO(select_date._d);
+        }
+    }
 	//繪製最初的svg，因為之後都只會更新dom，所以不會再呼叫他了！
 	function draw(data){
 	    // console.log(data);
@@ -89,8 +114,6 @@
         {
             detail[DB.serviceItems[foo]] = "";
         }
-        console.log(detail);
-        console.log(data);
         for(var foo in data)
         {
             var district ={};
@@ -106,7 +129,7 @@
                 district[data[foo].listData[bar].area]++;
             }
             // district = district.sort();
-            console.log(district);
+            // console.log(district);
             district_sorted = Object.keys(district).sort(function(a,b){return district[b]-district[a]})
             // console.log(district_sorted);
             // console.log(123);
@@ -118,7 +141,7 @@
                                         // + district_sorted[1] + ":" + district[district_sorted[1]] + "件<br>"
                                         // + district_sorted[2] + ":" + district[district_sorted[2]] + "件<br>");
         }
-        console.log(detail)
+        // console.log(detail)
 	    // console.log(detail);
 	    var filted_data = data_filter(data);
 	    // for(var foo in filter)
@@ -241,8 +264,6 @@
         {
             detail[DB.serviceItems[foo]] = "";
         }
-        console.log(detail);
-        console.log(data);
         for(var foo in data)
         {
             var district ={};
@@ -254,7 +275,6 @@
             {
                 district[data[foo].listData[bar].area]++;
             }
-            console.log(district);
             district_sorted = Object.keys(district).sort(function(a,b){return district[b]-district[a]})
             detail[data[foo].item] = district_sorted[0]  + district[district_sorted[0]] + "件<br>"
                                         + district_sorted[1]  + district[district_sorted[1]] + "件<br>"
@@ -385,27 +405,31 @@
 					if(dateGOGO(select_date._d) != dateGOGO(now_date._d))
 					{
 						var duration_set = moment.duration({'days' : 1});
-						console.log(select_date);
+						// console.log(select_date);
 						select_date = select_date.add(duration_set);
-						console.log(dateGOGO(select_date._d));
+						// console.log(dateGOGO(select_date._d));
 						d3.select('#show_date').text(dateGOGO(select_date._d));
-						now_bar_value++;
-						d3.select(".d3-slider-handle")
-							.style({
-								left: function(){
-									//更新bar的數值，sytle:"left"
-	//								console.log((now_bar_value / duration_day)*100 + "%")
-									return ((now_bar_value / duration_day)*100) + "%";
-								}
-							});
+                        console.log(slider_value);
+						slider_value = parseInt(slider_value) + (100/duration_day);
+                        console.log(slider_value);
+                        change_slider(slider_value);
+                        d3.select('input[type="range"]').style("left",slider_value + "%");
+	// 					d3.select(".d3-slider-handle")
+	// 						.style({
+	// 							left: function(){
+	// 								//更新bar的數值，sytle:"left"
+	// //								console.log((now_bar_value / duration_day)*100 + "%")
+	// 								return ((now_bar_value / duration_day)*100) + "%";
+	// 							}
+	// 						});
 					//TODO：改成api call的方式！
 	//					json_data = G.getItemsData('2016-11-09', dateGOGO(select_date._d), ['新化區', '新營區']);
-						d3.json("../../src/faked2.json", function(error, data){
-							if (error){
-								console.log(error);
-							}
-							change(data);
-						})
+						// d3.json("../../src/faked2.json", function(error, data){
+						// 	if (error){
+						// 		console.log(error);
+						// 	}
+						// 	change(data);
+						// })
 						if(dateGOGO(select_date._d) === dateGOGO(now_date._d)){
 							clearInterval(event);
 						}
@@ -423,21 +447,11 @@
 			.on("click", function(){
 			//因為stop，將bar_value改回0，並更新svg!
 				now_bar_value = 0;
-				d3.select('#show_date').text(dateGOGO(now_date._d));
-				d3.select(".d3-slider-handle")
-					.style({
-						left: "0%"
-
-					});
-			//TODO
-			//改成call api
-			//			json_data = G.getItemsData('2016-11-09', dateGOGO(now_date._d), ['新化區', '新營區']);
-				d3.json("../../src/faked.json", function(error, data){
-					if (error){
-						console.log(error);
-					}
-					change(data);
-				})
+				d3.select('#show_date').text(dateGOGO(select_date._d));
+                slider_value = 0;
+                console.log(slider_value);
+                change_slider(slider_value)
+                d3.select('input').attr('value',0);
 				clearInterval(event);
 			});
 		d3.select("#show_date").text()
