bf545cb28536326b9d85fd033655ffdd120954e2
TseHang <a54383813@gmail.com>
2016-11-19 08:38:47 +0800

    merge dev/merge

--- route.js
+++ route.js
@@ -10,20 +10,6 @@ var route = [
     partials: './partial.js',
     layout:  "./layout/index.hbs",
     filename: "./index.html"
-  },{
-    data: {
-      path: './',
-    },
-    partials: './partial.js',
-    layout:  "./layout/donutChart.hbs",
-    filename: "./donutChart.html"
-  },{
-    data: {
-      path: './',
-    },
-    partials: './partial.js',
-    layout:  "./layout/ching_map.hbs",
-    filename: "./ching_map.html"
   }
-];
+]
 module.exports = route;
